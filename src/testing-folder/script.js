100a528e76f984e6355b57214fd143ff03e25371
Tejas Lamba <73745640+TejasLamba2006@users.noreply.github.com>
2023-04-21 01:12:00 -0700

    Delete src directory

--- src/testing-folder/script.js
+++
@@ -1,8 +0,0 @@
-const { checkVote } = require("universe-list.js");
-
-voteFunction()
-
-async function voteFunction() {
-  const vote = await checkVote("1018001748020961311", "998763334264442912");
-  console.log(vote)
-}
